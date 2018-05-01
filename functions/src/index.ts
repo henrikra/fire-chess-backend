91d414b88f054ec531af16fd1e266f3d0c9dce80
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-05-01 22:54:18 +0300

    Use express to better handle cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from 'express';
 
 import calculateNewBoard from "./calculateNewBoard";
 import {
@@ -12,67 +13,68 @@ const cors = require("cors")({ origin: true });
 
 admin.initializeApp();
 
+const app = express();
+app.use(cors)
+
 const database = admin.firestore();
 const roomsRef = database.collection("rooms");
 
-exports.addRoom = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const isWhite = Math.random() < 0.5;
+app.get('/addRoom', (req, res) => {
+  const isWhite = Math.random() < 0.5;
 
-    roomsRef
-      .add({
-        moves: [],
-        [isWhite ? 'whitePlayerId' : 'blackPlayerId']: 'ofdsofk'
-      })
-      .then(doc => {
-        console.log("Document written with ID: ", doc.id);
-        res.send({ roomId: doc.id });
-      })
-      .catch(error => {
-        console.error("Error adding document: ", error);
-        res.status(500).send("fucked up");
-      });
-  });
+  roomsRef
+    .add({
+      moves: [],
+      [isWhite ? 'whitePlayerId' : 'blackPlayerId']: 'ofdsofk'
+    })
+    .then(doc => {
+      console.log("Document written with ID: ", doc.id);
+      res.send({ roomId: doc.id });
+    })
+    .catch(error => {
+      console.error("Error adding document: ", error);
+      res.status(500).send("fucked up");
+    });
 });
 
 const isAnyPiece = piece => isAnyBlackPiece(piece) || isAnyWhitePiece(piece);
 
-exports.movePiece = functions.https.onRequest((req, res) => {
-  cors(req, res, async () => {
-    const { from, to, roomId }: MovePieceRequest = req.body;
-    try {
-      const roomDoc = await roomsRef.doc(roomId).get();
-      const currentMoves = roomDoc.data().moves;
+app.post('/movePiece', async (req, res) => {
+  const { from, to, roomId }: MovePieceRequest = req.body;
+  try {
+    const roomDoc = await roomsRef.doc(roomId).get();
+    const currentMoves = roomDoc.data().moves;
 
-      // 1. calculate new board from doc.data().moves
-      const currentBoard = calculateNewBoard(initialBoard, currentMoves);
-      // 2. check if move is valid
-      const isMoveValid = checkIfMoveIsValid({ from, to }, currentBoard);
-      // 3. add new row to moves
+    // 1. calculate new board from doc.data().moves
+    const currentBoard = calculateNewBoard(initialBoard, currentMoves);
+    // 2. check if move is valid
+    const isMoveValid = checkIfMoveIsValid({ from, to }, currentBoard);
+    // 3. add new row to moves
 
-      if (isMoveValid) {
-        const numberOfPiecesInCurrentTable = currentBoard.filter(isAnyPiece)
-          .length;
-        const boardAfterMove = calculateNewBoard(currentBoard, [{ from, to }]);
-        const numberOfPiecesInTableAfterMove = boardAfterMove.filter(isAnyPiece)
-          .length;
-        const hasCaptureHappened =
-          numberOfPiecesInTableAfterMove < numberOfPiecesInCurrentTable;
+    if (isMoveValid) {
+      const numberOfPiecesInCurrentTable = currentBoard.filter(isAnyPiece)
+        .length;
+      const boardAfterMove = calculateNewBoard(currentBoard, [{ from, to }]);
+      const numberOfPiecesInTableAfterMove = boardAfterMove.filter(isAnyPiece)
+        .length;
+      const hasCaptureHappened =
+        numberOfPiecesInTableAfterMove < numberOfPiecesInCurrentTable;
 
-        try {
-          const newMoves = [...currentMoves, { from, to, hasCaptureHappened }];
-          await roomDoc.ref.update({ moves: newMoves });
-          res.send({ moves: newMoves });
-        } catch (error) {
-          console.error("Error updating moves", error);
-          res.status(500).send({ error: "Error updating moves" });
-        }
-      } else {
-        res.status(409).send({ error: "Invalid move" });
+      try {
+        const newMoves = [...currentMoves, { from, to, hasCaptureHappened }];
+        await roomDoc.ref.update({ moves: newMoves });
+        res.send({ moves: newMoves });
+      } catch (error) {
+        console.error("Error updating moves", error);
+        res.status(500).send({ error: "Error updating moves" });
       }
-    } catch (error) {
-      console.error("Error getting document: ", error);
-      res.status(500).send({ error: "Error getting document" });
+    } else {
+      res.status(409).send({ error: "Invalid move" });
     }
-  });
+  } catch (error) {
+    console.error("Error getting document: ", error);
+    res.status(500).send({ error: "Error getting document" });
+  }
 });
+
+exports.app = functions.https.onRequest(app);
