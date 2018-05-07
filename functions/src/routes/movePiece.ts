f2e3e659a7850338fc33f77d5b3574c5a1c491a6
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-05-07 20:07:22 +0300

    Allow only moving your own pieces

--- functions/src/routes/movePiece.ts
+++ functions/src/routes/movePiece.ts
@@ -1,7 +1,13 @@
 import { roomsRef } from "../database";
 import calculateNewBoard from "../calculateNewBoard";
 import { initialBoard } from "../initialBoard";
-import { checkIfMoveIsValid, isAnyBlackPiece, isAnyWhitePiece, isAnyPiece } from "../moveValidation";
+import {
+  checkIfMoveIsValid,
+  isAnyBlackPiece,
+  isAnyWhitePiece,
+  isAnyPiece,
+  squareToIndexOnBoard
+} from "../moveValidation";
 import { Request, Response } from "express";
 
 export default async (req: Request, res: Response) => {
@@ -32,10 +38,6 @@ export default async (req: Request, res: Response) => {
     }
 
     const isWhiteTurn = currentMoves.length % 2 === 0;
-    console.log("isWhiteTurn", isWhiteTurn);
-    console.log("userId", userId);
-    console.log("whitePlayerId", whitePlayerId);
-    console.log("blackPlayerId", blackPlayerId);
     const isBlackTurn = !isWhiteTurn;
     if (isWhiteTurn && userId !== whitePlayerId) {
       res
@@ -49,12 +51,22 @@ export default async (req: Request, res: Response) => {
         .send({ error: "You have to wait until black makes its move" });
       return;
     }
-    // 1. calculate new board from doc.data().moves
+
     const currentBoard = calculateNewBoard(initialBoard, currentMoves);
-    // check that you are moving only your own pieces
-    // 2. check if move is valid
+
+    const fromBoardIndex = squareToIndexOnBoard(from);
+    const fromChessPiece = currentBoard[fromBoardIndex];
+    if (
+      (isWhiteTurn && !isAnyWhitePiece(fromChessPiece)) ||
+      (isBlackTurn && !isAnyBlackPiece(fromChessPiece))
+    ) {
+      res
+        .status(403)
+        .send({ error: "You can only move your own chess pieces" });
+      return;
+    }
+
     const isMoveValid = checkIfMoveIsValid({ from, to }, currentBoard);
-    // 3. add new row to moves
 
     if (isMoveValid) {
       const numberOfPiecesInCurrentTable = currentBoard.filter(isAnyPiece)
@@ -74,10 +86,10 @@ export default async (req: Request, res: Response) => {
         res.status(500).send({ error: "Error updating moves" });
       }
     } else {
-      res.status(409).send({ error: "Invalid move" });
+      res.status(409).send({ error: "You can't do move like that" });
     }
   } catch (error) {
     console.error("Error getting document: ", error);
     res.status(500).send({ error: "Error getting document" });
   }
-}
+};
